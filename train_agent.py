ceca0f1682f7a8c50dee9d55b6df9f6f1ac2cb69
Aleksandar Nedaković <aleksaleks123@gmail.com>
2019-07-06 20:40:31 +0200

    New conv model
    Code cleaned also

--- train_agent.py
+++ train_agent.py
@@ -6,14 +6,17 @@ import tensorflow as tf
 import numpy as np
 import keras
 import sys
+import argparse
+import cv2
 
 
 class Agent():
 
-    def __init__(self, path=None, iter_num=0, ram=True, best_score = 0):
+    def __init__(self, path=None, iter_num=0, conv=False, best_score=0.0):
         self.num_of_games = 100000
         self.iter_num = iter_num
         self.best_score = best_score
+        self.conv = conv
 
         self.init_exploration = 1
         self.final_exploration = 0.1
@@ -24,8 +27,12 @@ class Agent():
         self.batch_size = 32
         self.gamma = 0.99
 
+        self.RMS_learning_rate = 0.00025
+        self.RMS_rho = 0.95
+        self.RMS_epsilon = 0.01
+
         self.RAM_SHAPE = (128,)
-        self.PP_FRAME_SHAPE = (105, 80, 1)
+        self.PP_FRAME_SHAPE = (84, 84, 1)
         self.action_space = np.array([0, 2, 3])
         self.activate_all_actions = np.ones((1, len(self.action_space)))
         self.activate_all_actions_batch = np.ones((self.batch_size, len(self.action_space)))
@@ -37,12 +44,12 @@ class Agent():
         if path is not None:
             self.model = keras.models.load_model(path)
             self.target_model = keras.models.load_model(path)
-            if ram:
+            if not conv:
                 self.env = gym.make('Breakout-ramDeterministic-v4')  # always skip 4 frames and no randomness
             else:
                 self.env = gym.make('BreakoutDeterministic-v4')
         else:
-            if ram:
+            if not conv:
                 self.model = self.make_model()
                 self.target_model = self.make_model()
                 self.target_model.set_weights(self.model.get_weights())  # so they have same weights
@@ -64,7 +71,7 @@ class Agent():
         filtered_output = keras.layers.multiply([output, actions_input])
 
         model = keras.models.Model(input=[ram_input, actions_input], output=filtered_output)
-        optimizer = keras.optimizers.RMSprop(lr=0.00025, rho=0.95, epsilon=0.01)
+        optimizer = keras.optimizers.RMSprop(lr=self.RMS_learning_rate, rho=self.RMS_rho, epsilon=self.RMS_epsilon)
         model.compile(optimizer, loss='mse')
         print(model.summary())
         return model
@@ -75,11 +82,11 @@ class Agent():
         normalized = keras.layers.Lambda(lambda x: x / 255.0)(frames_input)
 
         conv_1 = keras.layers.convolutional.Convolution2D(
-            16, 8, subsample=(4, 4), activation='relu'
+            16, 8, 8, input_shape=(1, 84, 84), subsample=(4, 4), activation='relu'
         )(normalized)
 
         conv_2 = keras.layers.convolutional.Convolution2D(
-            32, 4, subsample=(2, 2), activation='relu'
+            32, 4, 4, input_shape=(20, 20, 32), subsample=(2, 2), activation='relu'
         )(conv_1)
         conv_flattened = keras.layers.core.Flatten()(conv_2)
         hidden = keras.layers.Dense(256, activation='relu')(conv_flattened)
@@ -89,7 +96,7 @@ class Agent():
         filtered_output = keras.layers.multiply([output, actions_input])
 
         model = keras.models.Model(input=[frames_input, actions_input], output=filtered_output)
-        optimizer = keras.optimizers.RMSprop(lr=0.00025, rho=0.95, epsilon=0.01)
+        optimizer = keras.optimizers.RMSprop(lr=self.RMS_learning_rate, rho=self.RMS_rho, epsilon=self.RMS_epsilon)
         model.compile(optimizer, loss='mse')
 
         print(model.summary())
@@ -101,17 +108,23 @@ class Agent():
             self.env.reset()
             score = 0
             state, reward, terminal, info = self.env.step(1)
+            if self.conv:
+                state = self.preprocess(state)
             score += reward
             lives_left = info['ale.lives']
             while not terminal:
                 action = self.make_action(state, self.iter_num)
                 new_state, reward, terminal, info = self.env.step(action)
+                if self.conv:
+                    new_state = self.preprocess(new_state)
                 score += reward
                 if (info['ale.lives'] < lives_left):  # if life is lost
                     lives_left = info['ale.lives']
                     reward = -3
                     self.xp_memory.append((state, action, new_state, reward, terminal))
                     new_state, reward, terminal, _ = self.env.step(1)  # start again
+                    if self.conv:
+                        new_state = self.preprocess(new_state)
                     score += reward
                 else:
                     self.xp_memory.append((state, action, new_state, reward, terminal))
@@ -119,18 +132,15 @@ class Agent():
                     self.xp_memory.popleft()
                 state = new_state
                 self.iter_num += 1
-                if self.iter_num >= self.batch_size and self.iter_num % self.fit_model_after_frames == 0:
-                    try:
-                        self.fit(sample(self.xp_memory, self.batch_size))
-                    except ValueError:
-                        pass # if iter_num is bigger than batch size and xp_memory is still empty (when continuing training)
+                if self.iter_num % self.fit_model_after_frames == 0:
+                    self.fit(sample(self.xp_memory, min(self.batch_size, len(self.xp_memory))))
                 if self.iter_num % self.update_target_model_after_frames == 0:
                     self.target_model.set_weights(self.model.get_weights())
             scores.append(score)
-            if score>=self.best_score:
+            if score >= self.best_score:
                 self.best_score = score
                 print(f"New best score: {self.best_score}")
-                self.model.save(f"best_models/best_modeL_score_{int(self.best_score)}")
+                self.model.save(f"best_models/best_model_score_{int(self.best_score)}")
             if game_num % 100 == 0:
                 print(f"Game number: {game_num}\t\tAvg score:{np.mean(scores)}\t\tFrame:{self.iter_num}")
                 scores = []
@@ -139,13 +149,19 @@ class Agent():
                 self.model.save(f"models/final_model.mdl")
 
     def fit(self, batch):
+        if self.conv:
+            states = np.zeros((len(batch), self.PP_FRAME_SHAPE[0], self.PP_FRAME_SHAPE[1], self.PP_FRAME_SHAPE[2]),
+                              dtype=np.uint8)
 
-        states = np.zeros((len(batch), self.RAM_SHAPE[0]), dtype=np.uint8)
+            new_states = np.zeros((len(batch), self.PP_FRAME_SHAPE[0], self.PP_FRAME_SHAPE[1], self.PP_FRAME_SHAPE[2]),
+                                  dtype=np.uint8)
+        else:
+            states = np.zeros((len(batch), self.RAM_SHAPE[0]), dtype=np.uint8)
+            new_states = np.zeros((len(batch), self.RAM_SHAPE[0]), dtype=np.uint8)
+
         actions_encoded = np.zeros((len(batch), len(self.action_space)), dtype=np.uint8)
-        new_states = np.zeros((len(batch), self.RAM_SHAPE[0]), dtype=np.uint8)
         rewards = np.zeros((len(batch),), dtype=np.float32)
         terminals = np.zeros((len(batch),), dtype=np.bool)
-
         # copying by value
         for i in range(len(batch)):
             states[i] = batch[i][0]
@@ -173,83 +189,27 @@ class Agent():
             return self.action_space[
                 np.argmax(self.model.predict([np.expand_dims(state, axis=0), self.activate_all_actions]))]
 
-    def to_grayscale(self, img):
-        return np.mean(img, axis=2).astype(np.uint8)
-
-    def downsample(self, img):
-        return img[::2, ::2]
-
-    def preprocess(self, img):
-        return np.expand_dims(self.to_grayscale(self.downsample(img)), axis=2)
-
-    def train_conv(self):
-        scores = []
-        for game_num in range(self.num_of_games):
-            self.env.reset()
-            score = 0
-            state, reward, terminal, info = self.env.step(1)
-            state = self.preprocess(state)
-            score += reward
-            lives_left = info['ale.lives']
-            while not terminal:
-                action = self.make_action(state, self.iter_num)
-                new_state, reward, terminal, info = self.env.step(action)
-                new_state = self.preprocess(new_state)
-                score += reward
-                if (info['ale.lives'] < lives_left):  # if life is lost
-                    lives_left = info['ale.lives']
-                    reward = -3
-                    self.xp_memory.append((state, action, new_state, reward, terminal))
-                    new_state, reward, terminal, _ = self.env.step(1)  # start again
-                    new_state = self.preprocess(new_state)
-                    score += reward
-                else:
-                    self.xp_memory.append((state, action, new_state, reward, terminal))
-                if (len(self.xp_memory) > self.xp_length):
-                    self.xp_memory.popleft()
-                state = new_state
-                self.iter_num += 1
-                if self.iter_num >= self.batch_size and self.iter_num % self.fit_model_after_frames == 0:
-                    self.fit_conv(sample(self.xp_memory, self.batch_size))
-                if self.iter_num % self.update_target_model_after_frames == 0:
-                    self.target_model.set_weights(self.model.get_weights())
-            scores.append(score)
-            if game_num % 100 == 0:
-                print(f"Game number: {game_num}\t\tAvg score:{np.mean(scores)}\t\tFrame:{self.iter_num}")
-                scores = []
-                self.model.save(f"models/model_game_{game_num}.mdl")
-            if game_num == self.num_of_games - 1:
-                self.model.save(f"models/final_model.mdl")
-
-    def fit_conv(self, batch):
-
-        states = np.zeros((len(batch), self.PP_FRAME_SHAPE[0], self.PP_FRAME_SHAPE[1], self.PP_FRAME_SHAPE[2]), dtype=np.uint8)
-        actions_encoded = np.zeros((len(batch), len(self.action_space)), dtype=np.uint8)
-        new_states = np.zeros((len(batch), self.PP_FRAME_SHAPE[0], self.PP_FRAME_SHAPE[1], self.PP_FRAME_SHAPE[2]), dtype=np.uint8)
-        rewards = np.zeros((len(batch),), dtype=np.float32)
-        terminals = np.zeros((len(batch),), dtype=np.bool)
-
-        # copying by value
-        for i in range(len(batch)):
-            states[i] = batch[i][0]
-            actions_encoded[i][self.action_to_index[batch[i][1]]] = 1
-            new_states[i] = batch[i][2]
-            rewards[i] = batch[i][3]
-            terminals[i] = batch[i][4]
-
-        next_Q_values = self.target_model.predict([new_states, self.activate_all_actions_batch])
-        next_Q_values[terminals] = self.zero_q_values
-        #  Q(s,a) = r + gamma*max(Q(s',a'))
-        Q_values = rewards + self.gamma * np.max(next_Q_values, axis=1)
-
-        self.model.fit([states, actions_encoded], actions_encoded * np.expand_dims(Q_values, axis=1), nb_epoch=1,
-                       batch_size=self.batch_size, verbose=0)
+    # taken from http://www.pinchofintelligence.com/openai-gym-part-3-playing-space-invaders-deep-reinforcement-learning/
+    def preprocess(self, observation):
+        observation = cv2.cvtColor(cv2.resize(observation, (84, 110)), cv2.COLOR_BGR2GRAY)
+        observation = observation[26:110, :]
+        ret, observation = cv2.threshold(observation, 1, 255, cv2.THRESH_BINARY)
+        return np.reshape(observation, (84, 84, 1))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        a = Agent()
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-c", "--conv", action="store_true",
+                        help="use convolutional network on frame inputs")
+    parser.add_argument("-m", "--model",
+                        help="continue traning of a model")
+    args = parser.parse_args()
+
+    if args.model:
+        a = Agent(conv=args.conv, path=args.model, iter_num=1000000, best_score=350.0)
         a.train()
     else:
-        a = Agent(path=sys.argv[1], iter_num=1000000, best_score=350.0)
+        a = Agent(conv=args.conv)
         a.train()
